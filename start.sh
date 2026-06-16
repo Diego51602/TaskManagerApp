13fca8fddecd7bae2b7fea0ce570d53162316358
Diego51602 <136129720+Diego51602@users.noreply.github.com>
2026-06-16 17:28:18 -0600

    Fix port expansion in start script

--- start.sh
+++ start.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
-sed -i "s/listen 80/listen ${PORT:-5050}/g" /etc/nginx/conf.d/default.conf
+cat > /etc/nginx/conf.d/default.conf << EOF
+server {
+    listen ${PORT:-5050};
+    server_name localhost;
+    root /usr/share/nginx/html;
+    index index.html;
+
+    location / {
+        try_files \$uri \$uri/ /index.html;
+    }
+}
+EOF
 nginx -g 'daemon off;'
